da8824c3a69b19c4d3c7a8ffb1733ba2c97ed23e
John Perchun <evgeny_perchun@mail.ru>
2021-01-03 03:02:26 +0300

    commit for review 1.4

--- homework.py
+++ homework.py
@@ -1,30 +1,40 @@
 import time
 import os
 import requests
+
 from twilio.rest import Client
-from dotenv import load_dotenv 
+from dotenv import load_dotenv
+
+
 load_dotenv()
+VK_API_VERSION = os.getenv('VK_API_VERSION')
+VK_TOKEN = os.getenv('VK_TOKEN')
+TWILIO_CLIENT = Client(os.getenv('TWILIO_SID'), os.getenv('TWILIO_TOKEN'))
+NUMBER_FROM = os.getenv('NUMBER_FROM')
+NUMBER_TO = os.getenv('NUMBER_TO')
+API_URL = 'https://api.vk.com/method/users.get'
 
 
 def get_status(user_id):
     params = {
         'user_ids': user_id,
-        'v': '5.92',
-        'access_token': os.getenv('VK_TOKEN'),
+        'v': VK_API_VERSION,
+        'access_token': VK_TOKEN,
         'fields': 'online'
     }
-    user_status = requests.post('https://api.vk.com/method/users.get', params=params)
-    return user_status.json()['response'][0]['online']
+    try:
+        user_status = requests.post(API_URL, params=params)
+        return user_status.json().get('response')[0].get('online')
+    except requests.exceptions.RequestException as error:
+        print(error)
 
 # send_sms
 def sms_sender(sms_text):
-    account_sid = os.getenv('TWILIO_SID')
-    auth_token = os.getenv('TWILIO_TOKEN')
-    client = Client(account_sid, auth_token)
+    client = TWILIO_CLIENT
     message = client.messages.create(
         body=sms_text,
-        from_=os.getenv('NUMBER_FROM'),
-        to=os.getenv('NUMBER_TO')
+        from_=NUMBER_FROM,
+        to=NUMBER_TO
     )
     return message.sid
 
